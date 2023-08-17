110a036ca3e8f08ce7c4b0e8b355c9be755a1e6a
DavigJ <david.j.jung@gmail.com>
2023-08-17 16:11:20 -0400

    good start on morph data

--- kubejs/server_scripts/morph_medals.js
+++
@@ -1,7 +0,0 @@
-onEvent('item.right_click', (event) => {
-     if(event.item == 'kubejs:creeper_medal') {
-          event.server.runCommandSilent(`morph ${event.player.name.string} minecraft:creeper`);
-          event.player.addItemCooldown("kubejs:creeper_medal", 500);
-          event.player.sendInventoryUpdate();
-     }
-});
